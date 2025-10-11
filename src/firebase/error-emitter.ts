cd0438a80f19fb784efaf3ea016ababd174b38c7
reminderr Services <reminderr.services@gmail.com>
2025-10-11 09:39:23 +0000

    Application error: a client-side exception has occurred while loading 60

--- src/firebase/error-emitter.ts
+++ src/firebase/error-emitter.ts
@@ -1,8 +1,34 @@
 'use client';
 
-import { EventEmitter } from 'events';
+// A simple, browser-compatible event emitter.
+type Listener = (...args: any[]) => void;
 
-// It's a bit of a hack to use 'events' but it's a small dependency and works well for this.
-class ErrorEmitter extends EventEmitter {}
+class ErrorEmitter {
+  private events: { [key: string]: Listener[] } = {};
+
+  on(event: string, listener: Listener): this {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+    return this;
+  }
+
+  emit(event: string, ...args: any[]): boolean {
+    if (!this.events[event]) {
+      return false;
+    }
+    this.events[event].forEach(listener => listener(...args));
+    return true;
+  }
+
+  off(event: string, listener: Listener): this {
+    if (!this.events[event]) {
+      return this;
+    }
+    this.events[event] = this.events[event].filter(l => l !== listener);
+    return this;
+  }
+}
 
 export const errorEmitter = new ErrorEmitter();
