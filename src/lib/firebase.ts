65a095c97e42686ead0555a3b684e1333678bf02
nischu <reminderr.services@gmail.com>
2025-11-26 06:40:10 +0530

    Fix Storage bucket name to use firebasestorage.app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCjiBKKDIylbYFAi0aNYxgSYkviWPAF_ww",
   authDomain: "studio-4485772157-5c03f.firebaseapp.com",
   projectId: "studio-4485772157-5c03f",
-  storageBucket: "studio-4485772157-5c03f.appspot.com",
+  storageBucket: "studio-4485772157-5c03f.firebasestorage.app",
   messagingSenderId: "639742127067",
   appId: "1:639742127067:web:9db7a19cced9e33cb5d5fe"
 };
